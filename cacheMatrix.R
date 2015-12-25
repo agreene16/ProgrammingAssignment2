e6cc19686680bb5d41aa498d77bbe40747fdcaa8
agreene16 <agreene16@wooster.edu>
2015-12-25 15:28:17 -0500

    Rename cachematrix.R to cacheMatrix.R

---
+++ cacheMatrix.R
@@ -0,0 +1,30 @@
+#The first function creates a list that contains various functions, and the second function takes this input to solve or simply retrieve#
+#an output based on the initial input x#
+
+## makeCacheMatrix takes the input of matrix x and creates a list of functions that will operate within the environment x when
+## evaluated by cachesolve
+makeCacheMatrix <- function(x=matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <- solve
+        getinverse <- function() m 
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+}
+
+## cachesolve takes the output of makeCacheMatrix, an atomic vector of functions operating within environment x, and thereby either 
+## retrieves an inverted matrix that exists as cached data, or solves an invertible matrix that has not yet been solved
+cachesolve <- function(x, ...) {
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+}
